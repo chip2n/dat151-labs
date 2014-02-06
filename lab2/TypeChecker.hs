5f4cfc1ed3fe88aa9c731e9e907b31d077d103b7
Andreas Arvidsson <andreas.arvidson@gmail.com>
2014-02-06 23:20:33 +0100

    Added skeleton for lab2

--- lab2/TypeChecker.hs
+++ lab2/TypeChecker.hs
@@ -7,3 +7,41 @@ import ErrM
 
 typecheck :: Program -> Err ()
 typecheck p = return ()
+
+checkDefs :: Env -> [Def] -> Err ()
+checkDefs = undefined
+
+checkDef :: Env -> Degf -> Err Env
+checkDef = undefined
+
+checkStms :: Env -> [Stm] -> Err ()
+checkStms = undefined
+
+checkStm :: Env -> Stm -> Err Env
+checkStm = undefined
+
+checkExp :: Env -> Exp -> Type -> Err ()
+checkExp = undefined
+
+inferExp :: Env -> Exp -> Err Type
+inferExp = undefined
+
+
+
+
+
+
+
+type Env = [[(Ident, Type)]]
+
+emptyEnv :: Env
+emptyEnv = [[]]
+
+addVar :: Env -> Ident -> Type -> Err Env
+addVar = undefined
+
+lookupVar :: Env -> Ident -> Type -> Err Env
+lookupVar = undefined
+
+addScope :: Env -> Env
+addScope = undefined
