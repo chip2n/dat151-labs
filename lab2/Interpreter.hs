e3e4a97c6c666896ff6fe4c1a5bd70b4a00bb485
Andreas Arvidsson <andreas.arvidson@gmail.com>
2014-02-13 18:53:47 +0100

    Continuing with interpreter.

--- lab2/Interpreter.hs
+++ lab2/Interpreter.hs
@@ -8,7 +8,14 @@ import Control.Monad
 import qualified Data.Map as M
 
 interpret :: Program -> IO ()
-interpret (PDefs defs) = putStrLn "no interpreter yet"
+interpret (PDefs defs) = do
+    let env = foldl setFun emptyEnv defs
+    evalExp env (EApp (Id "main") [])
+    return ()
+
+execDef :: Env -> Def -> IO (Value, Env)
+execDef env (DFun t i args stms) = undefined
+    
 
 execStms :: Env -> [Stm] -> IO Env
 execStms env [] = return env
@@ -16,28 +23,46 @@ execStms env (s:stms) = do env' <- execStm env s
                            execStms env' stms
 
 execStm :: Env -> Stm -> IO Env
-execStm env s = do
+execStm env s =
     case s of
-        SExp e           -> evalExp env e >> return env
-        SDecls t xs      -> undefined
+        SExp e           -> evalExp env e >>= return . snd
+        SDecls _ xs      -> return $ foldl (addVar) env xs
         SInit t i e      -> do
             let env' = addVar env i
             (value, env'') <- evalExp env' e
             return $ setVar env'' i value
-        SReturn e        -> undefined
-        SWhile e s1      -> undefined
-        SBlock xs        -> execStms (enterScope env) xs
-        SIfElse e s1 s2  -> undefined
+        SReturn e        -> return env
+        SWhile e s1      -> do
+            (val, env') <- evalExp env e
+            if val == VBool True
+                then do 
+                    env'' <- execStm env' s1
+                    execStm env'' s1
+                else return env' 
+        SBlock xs        -> do
+            env' <- execStms (enterScope env) xs
+            return (leaveScope env')
+        SIfElse e s1 s2  -> do
+            (val, env') <- evalExp env e
+            if val == VBool True
+                then execStm env' s1
+                else execStm env' s2
 
 evalExp :: Env -> Exp -> IO (Value,Env)
 evalExp env e = 
     case e of
-        ETrue         -> undefined
-        EFalse        -> undefined
-        EInt i        -> undefined
-        EDouble d     -> undefined
+        ETrue         -> return (VBool True, env)
+        EFalse        -> return (VBool False, env)
+        EInt i        -> return (VInt i, env)
+        EDouble d     -> return (VDouble d, env)
         EId x         -> return (lookupVar env x, env)
-        EApp i es     -> undefined
+        EApp i es     -> do
+            env' <- foldM evalExp env es
+            let (DFun _ _ args stms) = lookupFun env' i
+            let args' = map (\(ADecl at it) -> it) args
+
+            return undefined
+             
         EPIncr e      -> undefined
         EPDecr e      -> undefined
         EIncr e       -> undefined
@@ -67,6 +92,7 @@ data Value = VInt Integer
            | VBool Bool
            | VVoid
            | VUndef
+           deriving (Eq, Show)
 
 emptyEnv :: Env
 emptyEnv = undefined
@@ -77,8 +103,14 @@ addVar = undefined
 setVar :: Env -> Id -> Value -> Env
 setVar = undefined
 
+setFun :: Env -> Def -> Env
+setFun = undefined
+
 lookupVar :: Env -> Id -> Value
 lookupVar = undefined
+
+lookupFun :: Env -> Id -> Def
+lookupFun = undefined
 
 enterScope :: Env -> Env
 enterScope = undefined
