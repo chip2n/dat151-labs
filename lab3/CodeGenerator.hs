877eab14060fbc749744b0f7260f05bb2c568cc3
Andreas Arvidsson <andreas.arvidson@gmail.com>
2014-02-15 13:30:39 +0100

    Added basis for lab3.

---
+++ lab3/CodeGenerator.hs
@@ -0,0 +1,35 @@
+module CodeGenerator where
+
+import qualified Data.Map as Map
+import Control.Monad
+import Control.Monad.State
+
+import AbsCPP
+
+compileStm :: Stm -> State Env ()
+compileStm s = undefined
+
+compileExp :: Exp -> State Env ()
+compileExp e = undefined
+
+
+emit :: Instruction -> State Env ()
+emit i = modify (\s -> s {code = i : code s})
+
+
+
+
+data Env = Env { vars :: [Map.Map Id Int]
+               , maxvar :: Int
+               , code :: [Instruction]
+               } deriving (Show)
+
+type Instruction = Integer
+
+
+
+lookupVar :: Id -> State Env Int
+lookupVar x = do
+    s <- get
+    -- then look up the first occurence of x in (vars s)
+    undefined
